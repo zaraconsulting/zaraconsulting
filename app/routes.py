3cc63b2596e9f5f088db8155a338f6204ac5f660
derekmhawkins <derekhawkins.tech@gmail.com>
2019-03-13 09:49:35 -0500

    general updates to dynamic information on website

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
 from flask import render_template, redirect, url_for
 
+@app.context_processor
+def get_info():
+  return dict(
+    company_email=app.config['COMPANY_EMAIL'],
+    company_phone=app.config['COMPANY_PHONE'],
+    company_name_short="Zara Consulting",
+    company_name_long="Zara Consulting, Inc.",
+  )
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+  SECRET_KEY = os.environ.get('SECRET_KEY') or os.getenv('SECRET_KEY')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY") or os.getenv("GOOGLE_MAPS_API_KEY")
+  PRES_EMAIL = os.environ.get('PRES_EMAIL') or os.getenv('PRES_EMAIL')
+  COMPANY_EMAIL = os.environ.get('COMPANY_EMAIL') or os.getenv('COMPANY_EMAIL')
+  COMPANY_PHONE = os.environ.get('COMPANY_PHONE') or os.getenv('COMPANY_PHONE')
+  MAIL_API_KEY = os.environ.get('MAIL_API_KEY') or os.getenv('MAIL_API_KEY')
+  MAIL_DOMAIN = os.environ.get('MAIL_DOMAIN') or os.getenv('MAIL_DOMAIN')
+  IMAGE_FOLDER = os.path.join(basedir, 'app/blueprints/blog/static/uploads')
+  MAIL_ACCOUNT = os.environ.get('MAIL_ACCOUNT') or os.getenv('MAIL_ACCOUNT')
